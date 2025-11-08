6597995d197f15a4d3986920a4c6f35d15bdc8de
precision-sys <tiana2412@gmail.com>
2025-11-08 18:06:41 +0300

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. Better luck next time!");
+
+        }
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("YOur grade is B");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+
+        else
+        {
+
+            Console.WriteLine("Your grade is F");
+        }
+
     }
 }
