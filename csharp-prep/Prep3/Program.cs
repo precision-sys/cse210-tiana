b20078d307cf143b2cbabff615803091c700da26
precision-sys <tiana2412@gmail.com>
2025-11-09 04:40:41 +0300

    prep1 to prep5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,23 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
+
+
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int userGuess = int.Parse(Console.ReadLine());
+        int userGuess = -1;
 
-        if (userGuess < magicNumber)
+
+        while (userGuess != magicNumber)
         {
-            Console.WriteLine("higher" );
-        }
-        else if (userGuess > magicNumber)
-        {
-            Console.WriteLine("lower" );
-        }
-        else
-        {
-            Console.WriteLine("Congratulations! You guessed the magic number!");
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("higher");
+
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number!");
+            }
         }
     }
 }
+
