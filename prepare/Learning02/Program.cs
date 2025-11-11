b55d2ab58bb1fdb31e5643a623dc7900ffe4e5fd
precision-sys <tiana2412@gmail.com>
2025-11-11 17:18:16 +0300

    resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new job();
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Corp";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Innovatech";
+        job2._startYear = 2023;
+        job2._endYear = 2025;   
+
+        Resume myResume = new Resume();
+        myResume._name = "Tiana Andry";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
     }
 
 
